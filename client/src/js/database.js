547747995b9009bc8ef3c0e4b8b311772eee440a
Acquah <emanuel.molina2k@gmail.com>
2023-06-15 22:49:03 -0700

    add configuration to worbox plugin, and css loader

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,6 +14,7 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 // export const putDb = async (content) => console.error('putDb not implemented');
+//done
 export const putDb = async (content) => {
   const db = await openDB('jate', 1);
   const transaction = db.transaction('jate', 'readwrite');
@@ -27,6 +28,7 @@ export const putDb = async (content) => {
 
 // TODO: Add logic for a method that gets all the content from the database
 // export const getDb = async () => console.error('getDb not implemented');
+//done
 
 // initdb();
 export const getDb = async () => {
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -5,6 +5,7 @@ const { InjectManifest } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
+//done
 
 module.exports = () => {
   return {
@@ -18,12 +19,51 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        chunks: ['main'],
+      }),
+      new HtmlWebpackPlugin({
+        filename: 'install.html',
+        template: './src/install.html',
+        chunks: ['install'],
+      }),
+      new WebpackPwaManifest({
+        filename: 'manifest.json',
+        name: 'My App',
+        short_name: 'App',
+        description: 'My Progressive Web App',
+        background_color: '#ffffff',
+        theme_color: '#000000',
+        icons: [
+          {
+            src: path.resolve('src/assets/icon.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
+      new InjectManifest({
+        swSrc: './src/sw.js',
+      }),
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },
       ],
     },
   };
